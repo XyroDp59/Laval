7d42f6068b6e6793cdb3b9403b93de43d88241cf
XyroDp59 <81575508+XyroDp59@users.noreply.github.com>
2025-04-07 18:00:47 +0200

    cumulonimbused

--- Assets/Project/Scripts/clouds/Cumulonimbus.cs
+++ Assets/Project/Scripts/clouds/Cumulonimbus.cs
@@ -6,17 +6,17 @@ public class Cumulonimbus : MonoBehaviour
 {
     [SerializeField] Cloud cloudPrefab;
     [SerializeField] float radius;
-    [SerializeField] float density;
+    [SerializeField] float number;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i< Mathf.PI * radius*radius*density; i++)
+        for (int i = 0; i< number * radius *radius; i++)
         {
             float r = Random.value*radius;
             float theta = Random.value*2*Mathf.PI;
             float variation = (Random.value - 0.5f) * 2f;
-            Instantiate(cloudPrefab, new Vector3(r * Mathf.Cos(theta), transform.position.y, r * Mathf.Cos(theta)), Quaternion.identity);
+            Instantiate(cloudPrefab, new Vector3(r * Mathf.Cos(theta), transform.position.y, r * Mathf.Sin(theta)), Quaternion.identity);
         }
         Destroy(gameObject, 1f);
     }
