3adf73e288580b9f2a141eb0e4d1bf6ed6000c54
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-04-08 11:51:02 +0200

    Pharaon fix

--- Assets/Project/Scripts/Pharaon/Pharaon.cs
+++ Assets/Project/Scripts/Pharaon/Pharaon.cs
@@ -1,22 +1,25 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Pharaon : MonoBehaviour
 {
-    [SerializeField] private GameObject goldParticles;
+    [SerializeField] private ParticleSystem goldParticles;
+    [SerializeField] private CapsuleCollider particlesCollider;
     [SerializeField] private float minSpeed;
     
     private bool rightHanded;
     private HandAnimator rightHandAnimator;
     private HandAnimator leftHandAnimator;
     private HandAnimator currentHandAnimator;
-    private Rigidbody rb;
+    private XRControllerSpeed controller;
     private bool beingHeld;
-
+    
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rightHandAnimator = GameObject.FindGameObjectWithTag("RightHand").GetComponent<HandAnimator>();
+        leftHandAnimator = GameObject.FindGameObjectWithTag("LeftHand").GetComponent<HandAnimator>();
     }
 
     public void OnGrab(SelectEnterEventArgs args)
@@ -24,6 +27,7 @@ public class Pharaon : MonoBehaviour
         rightHanded = args.interactorObject.transform.parent.name.Contains("Right");
         currentHandAnimator = rightHanded ? rightHandAnimator : leftHandAnimator;
         currentHandAnimator.SetEgyptianGrabbed(true);
+        controller = currentHandAnimator.gameObject.transform.parent.GetComponent<XRControllerSpeed>();
         beingHeld = true;
     }
 
@@ -35,13 +39,24 @@ public class Pharaon : MonoBehaviour
 
     private void Update()
     {
-        Debug.Log(rb.velocity.magnitude);
-        /*if (!beingHeld)
+        if (!beingHeld)
         {
-            goldParticles.SetActive(false);
+            particlesCollider.enabled = false;
+            var module = goldParticles.emission;
+            module.enabled = false;
             return;
-        }*/
+        }
 
-        goldParticles.SetActive(rb.velocity.magnitude > minSpeed);
+        if (controller.GetControllerSpeed().magnitude > minSpeed) StartCoroutine(ParticlesActivation());
+    }
+
+    private IEnumerator ParticlesActivation()
+    {
+        var module = goldParticles.emission;
+        module.enabled = true;
+        particlesCollider.enabled = true;
+        yield return new WaitForSeconds(2f);
+        module.enabled = false;
+        particlesCollider.enabled = false;
     }
 }
