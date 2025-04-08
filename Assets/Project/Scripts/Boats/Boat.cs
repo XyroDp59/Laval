131b68d1bf5627f134df24ec2b551ceaa79274e4
Wangha <132218157+Arwangha@users.noreply.github.com>
2025-04-08 14:54:14 +0200

    Player in main

--- Assets/Project/Scripts/Boats/Boat.cs
+++ Assets/Project/Scripts/Boats/Boat.cs
@@ -7,26 +7,41 @@ public class Boat : MonoBehaviour
     private NavMeshAgent agent;
     private Vector3 targetPosition = Vector3.zero;
     NavMeshPath path;
+    
+    [SerializeField] private float minSpeed;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.TryGetComponent(out XRControllerSpeed controller))
+        {
+            HandAnimator handAnimator = other.GetComponentInChildren<HandAnimator>();
+            if (!handAnimator) return;
+            if (controller.GetControllerSpeed().magnitude > minSpeed && handAnimator.ClosedFist())
+            {
+                handAnimator.Rumble(1, 0.2f);
+                Destroy(gameObject);
+            }
+        }
+    }
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         path = new NavMeshPath();
     }
-
+    /*
     private void FixedUpdate()
     {
         if (agent.CalculatePath(targetPosition, path) && targetPosition != Vector3.zero) return;
         foreach (var bridge in UnbreakableBridges.Bridges)
         {
-            if (agent.CalculatePath(bridge.transform.position, path))
+            if (agent.CalculatePath(bridge.position, path))
             {
-                targetPosition = bridge.transform.position;
-                agent.SetDestination(targetPosition);
+                agent.SetDestination(bridge.position);
                 Debug.Log(bridge.name);
+                targetPosition = bridge.position;
                 return;
             }
             Debug.Log("no destination");
         }
-    }
+    }*/
 }
