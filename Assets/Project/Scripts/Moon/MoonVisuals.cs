a577cd795559047ad19f87b1644184186ed3806e
XyroDp59 <81575508+XyroDp59@users.noreply.github.com>
2025-04-08 15:42:34 +0200

    Mooning deez nuts

--- Assets/Project/Scripts/Moon/MoonVisuals.cs
+++ Assets/Project/Scripts/Moon/MoonVisuals.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(GameEvent))]
 public class MoonVisuals : MonoBehaviour
 {
     [SerializeField] private Transform moonParent;
+    [SerializeField] private GameEvent gameEvent;
+
+    private bool isNight = false;
+
+    private void Awake()
+    {
+        gameEvent = GetComponent<GameEvent>();
+    }
 
     private void Update()
     {
@@ -15,5 +24,19 @@ public class MoonVisuals : MonoBehaviour
         angles.z = moonParent.rotation.eulerAngles.z;
         rotation.eulerAngles = angles;
         transform.rotation = rotation;
+
+        if(transform.position.y < 0 && isNight)
+        {
+            // le jour
+            isNight = false;
+            gameEvent.RemoveEventFromQuarry();
+            //retirer event
+        }
+        else if ( !isNight )
+        {
+            isNight = true;
+            gameEvent.AddEventToQuarry();
+            //add event
+        }
     }
 }
